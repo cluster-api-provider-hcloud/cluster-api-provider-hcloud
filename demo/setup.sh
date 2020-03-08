838363a793e9bd8efa5fe3babcaed7c58e847887
Christian Simon <simon@swine.de>
2020-03-08 18:55:07 +0000

    Upgrade to the latest version

--- demo/setup.sh
+++ demo/setup.sh
@@ -7,7 +7,7 @@ set -x
 kind create cluster --name capi-hetzner || true
 
 # Install cluster api manager
-kubectl apply -f https://github.com/kubernetes-sigs/cluster-api/releases/download/v0.2.8/cluster-api-components.yaml
+kubectl apply -f https://github.com/kubernetes-sigs/cluster-api/releases/download/v0.2.10/cluster-api-components.yaml
 
 # Install kubeadm bootstrap provider
-kubectl apply -f https://github.com/kubernetes-sigs/cluster-api-bootstrap-provider-kubeadm/releases/download/v0.1.5/bootstrap-components.yaml
+kubectl apply -f https://github.com/kubernetes-sigs/cluster-api-bootstrap-provider-kubeadm/releases/download/v0.1.6/bootstrap-components.yaml
