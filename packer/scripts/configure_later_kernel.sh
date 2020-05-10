44d0ad841320cf9b82e4f9ea5dbb60207c71e81e
Christian Simon <simon@swine.de>
2020-05-10 13:51:32 +0100

    Pin old release of slight newer kernel

--- packer/scripts/configure_later_kernel.sh
+++ packer/scripts/configure_later_kernel.sh
@@ -7,7 +7,7 @@ set -o pipefail
 cat > /etc/yum.repos.d/CentOS-AltArch-Kernel.repo <<"EOF"
 [altarch-kernel]
 name=CentOS-$releasever AltArch - Kernel
-baseurl=http://mirror.centos.org/altarch/$releasever/kernel/$basearch/
+baseurl=http://vault.centos.org/altarch/7.7.1908/kernel/$basearch/
 gpgcheck=1
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
 EOF
