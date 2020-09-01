a3e653b634ef72790ef5aa01bf879fa3e2216cdf
Janis Kemper <janis.kemper@syself.com>
2020-09-01 20:18:32 +0200

    Removed server IP address from API server allowed IP addresses

--- pkg/cloud/resources/server/server.go
+++ pkg/cloud/resources/server/server.go
@@ -415,8 +415,10 @@ func (s *Service) Reconcile(ctx context.Context) (_ *ctrl.Result, err error) {
 		}
 
 		clientConfig, err := s.scope.ClientConfigWithAPIEndpoint(clusterv1.APIEndpoint{
-			Host: address.Address,
-			Port: s.scope.ControlPlaneAPIEndpointPort(),
+			/*
+				Host: address.Address,
+				Port: s.scope.ControlPlaneAPIEndpointPort(),
+			*/
 		})
 		if err != nil {
 			return nil, err
