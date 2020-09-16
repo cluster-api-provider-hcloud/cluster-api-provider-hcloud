917a568745f4542b24bab839e1aa564e27801d8b
batistein <sven.batista-steinbach@syself.com>
2020-09-16 21:24:57 +0200

    adding bastion host scripts

--- packer/centos-8_bastion-host/scripts/configure_base.sh
+++ packer/centos-8_bastion-host/scripts/configure_base.sh
@@ -46,10 +46,6 @@ EOF
 
 
 
-
-# Add Extra Packages for Enterprise Linux (EPEL) 8
-dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
-
 # Enable Repos
 dnf config-manager --set-enabled AppStream BaseOS extras PowerTools 
 
@@ -63,36 +59,3 @@ dnf -y install \
     nfs-utils \
     iscsi-initiator-utils \
     firewalld
-
-# disable portmapper rpcbind
-systemctl disable rpcbind.service rpcbind.socket
-
-# disable firewalld
-systemctl disable firewalld.service
-
-# disable kdump service
-systemctl disable kdump.service
-
-# mount bpfs for calico
-cat > /etc/systemd/system/sys-fs-bpf.mount <<EOF
-[Unit]
-Description=Cilium BPF mounts
-Documentation=http://docs.cilium.io/
-DefaultDependencies=no
-Before=local-fs.target umount.target
-After=swap.target
-
-[Mount]
-What=bpffs
-Where=/sys/fs/bpf
-Type=bpf
-Options=rw,nosuid,nodev,noexec,relatime,mode=700
-
-[Install]
-WantedBy=multi-user.target
-EOF
-systemctl enable sys-fs-bpf.mount
-
-# Set SELinux in enforcing mode (effectively disabling it)
-setenforce 1
-sed -i 's/^SELINUX=permissive\$/SELINUX=enforcing/' /etc/selinux/config
