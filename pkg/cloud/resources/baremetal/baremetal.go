8cd307b72971fb072d1c4e05580cd6668cedd914
Janis Kemper <janis.kemper@syself.com>
2020-10-28 15:58:29 +0100

    Add SSH commands sleep around reboot

--- pkg/cloud/resources/baremetal/baremetal.go
+++ pkg/cloud/resources/baremetal/baremetal.go
@@ -289,12 +289,24 @@ func (s *Service) provisionMachine(ctx context.Context, server models.Server) er
 		return errors.Errorf("Unable to activate rescue system: ", err)
 	}
 
+	// avoid errors when reboot comes too early for the previous command
+	_, _, err = runSSH("sleep 10", server.ServerIP, 22, privateSSHKey)
+	if err != nil {
+		return errors.Errorf("Error running the ssh command sleep 10: Error: %s, stderr: %s", err, stderr)
+	}
+
 	// Reset the server
 	stdout, stderr, err := runSSH("reboot", server.ServerIP, 22, privateSSHKey)
 	if err != nil {
 		if !strings.Contains(err.Error(), "exited without exit status or exit signal") {
 			return errors.Errorf("Error running the ssh command reboot: Error: %s, stderr: %s", err, stderr)
 		}
+	}
+
+	// wait for reboot
+	_, _, err = runSSH("sleep 30", server.ServerIP, 22, privateSSHKey)
+	if err != nil {
+		return errors.Errorf("Error running the ssh command sleep 30: Error: %s, stderr: %s", err, stderr)
 	}
 
 	// Find out if rescue system has been started successfully
